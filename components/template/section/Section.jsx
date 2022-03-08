cfcbee7c1da5f90721a946c6a56eb507fe79f870
Camille Sylla <camillesylla.web@gmail.com>
2022-03-08 14:24:39 +0100

    media querys

--- components/template/section/Section.jsx
+++ components/template/section/Section.jsx
@@ -3,7 +3,7 @@ import style from './section.module.scss'
 export default function Section ({height, children}) {
 
     return( 
-        <section style={{height : `${height ? height : "100vh"}`}}className={style.wrapper}>
+        <section className={style.wrapper}>
             {children}
         </section>
     )
